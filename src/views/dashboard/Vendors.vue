9bfbcf280b1051a1656c2377f397b03fb5aa8013
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2022-09-22 20:49:37 +0600

    part 72 done

--- src/views/dashboard/Vendors.vue
+++ src/views/dashboard/Vendors.vue
@@ -1,3 +1,49 @@
 <template>
-  <h2>Vendors</h2>
+  <div class="d-flex jc-between ai-center">
+    <h2>All vendors</h2>
+    <TheButton @click="addModal = true">Add New</TheButton>
+  </div>
+
+  <table class="mt-4">
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="i in 9" :key="i">
+        <td>Name</td>
+        <td>description</td>
+        <td>
+          <img src="/img/edit.png" alt="" class="action-icon" />
+          <img
+            src="/img/trash.png"
+            alt=""
+            class="action-icon action-icon--delete ml-3"
+          />
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <TheModal v-model="addModal" heading="Add new vendor">
+    <h2>Enter vendor details</h2>
+  </TheModal>
 </template>
+
+<script>
+import TheButton from "../../components/TheButton.vue";
+import TheModal from "../../components/TheModal.vue";
+
+export default {
+  data: () => ({
+    addModal: false
+  }),
+  components: {
+    TheButton,
+    TheModal
+  }
+};
+</script>
