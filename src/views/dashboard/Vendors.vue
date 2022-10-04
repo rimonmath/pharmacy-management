50b289f1ec8117565f91dfb60894ae4de4dcc28e
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2022-10-04 20:25:45 +0600

    vendor crud part 1 done

--- src/views/dashboard/Vendors.vue
+++ src/views/dashboard/Vendors.vue
@@ -13,9 +13,11 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="i in 9" :key="i">
-        <td>Name</td>
-        <td>description</td>
+      <tr v-for="vendor in vendors" :key="vendor.name">
+        <td>
+          {{ vendor.name }}
+        </td>
+        <td>{{ vendor.description }}</td>
         <td>
           <img src="/img/edit.png" alt="" class="action-icon" />
           <img
@@ -29,21 +31,112 @@
   </table>
 
   <TheModal v-model="addModal" heading="Add new vendor">
-    <h2>Enter vendor details</h2>
+    <form @submit.prevent="addNew">
+      <label class="block">Vendor Name</label>
+      <input
+        type="text"
+        placeholder="Enter vendor name"
+        class="mt-1 w-100"
+        v-model="newVendor.name"
+        required
+      />
+
+      <label class="block mt-3">Description</label>
+      <input
+        type="text"
+        placeholder="Write vendor short description"
+        class="mt-1 w-100"
+        v-model="newVendor.description"
+        required
+      />
+
+      <TheButton class="w-100 mt-4" :loading="adding"> Add </TheButton>
+    </form>
   </TheModal>
 </template>
 
 <script>
 import TheButton from "../../components/TheButton.vue";
 import TheModal from "../../components/TheModal.vue";
+import axios from "axios";
 
 export default {
   data: () => ({
-    addModal: false
+    addModal: false,
+    vendors: [],
+    newVendor: {
+      name: "",
+      description: ""
+    },
+    adding: false
   }),
   components: {
     TheButton,
     TheModal
+  },
+  mounted() {
+    this.getAllVendors();
+  },
+  methods: {
+    getAllVendors() {
+      axios
+        .get(
+          "http://localhost:3009/api/pharmacy-management/v1/private/vendor",
+          {
+            headers: {
+              authorization:
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjQ4OTM0OTIsImRhdGEiOnsib3JkZXJJZCI6IjYyZmQ4NTQ1YmI4YWFkM2JlNDJhN2YzNSIsInVzZXJuYW1lIjoidGVzdHVzZXIiLCJzdGF0dXMiOiJBY3RpdmUifSwiaWF0IjoxNjY0ODg5ODkyfQ.gAjmeg_rHt5yhidMhcA2q4NbowfczfuDI5w-txPXKXo"
+            }
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+          this.vendors = res.data;
+        })
+        .catch((err) => {
+          let errorMessage = "Something went wrong!";
+          if (err.response) {
+            errorMessage = err.response.data.message;
+          }
+
+          this.$eventBus.emit("toast", {
+            type: "Error",
+            message: errorMessage
+          });
+        });
+    },
+    addNew() {
+      // console.log(this.newVendor);
+      this.adding = true;
+      axios
+        .post(
+          "http://localhost:3009/api/pharmacy-management/v1/private/vendor",
+          this.newVendor,
+          {
+            headers: {
+              authorization:
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjQ4OTM0OTIsImRhdGEiOnsib3JkZXJJZCI6IjYyZmQ4NTQ1YmI4YWFkM2JlNDJhN2YzNSIsInVzZXJuYW1lIjoidGVzdHVzZXIiLCJzdGF0dXMiOiJBY3RpdmUifSwiaWF0IjoxNjY0ODg5ODkyfQ.gAjmeg_rHt5yhidMhcA2q4NbowfczfuDI5w-txPXKXo"
+            }
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+        })
+        .catch((err) => {
+          let errorMessage = "Something went wrong!";
+          if (err.response) {
+            errorMessage = err.response.data.message;
+          }
+
+          this.$eventBus.emit("toast", {
+            type: "Error",
+            message: errorMessage
+          });
+        })
+        .finally(() => {
+          this.adding = false;
+        });
+    }
   }
 };
 </script>
