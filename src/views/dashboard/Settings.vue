d9c3a30e4c7d4ca463d0f6d213ec5768807b570e
Md. Mamunur Rashid Rimon <rimonmath@gmail.com>
2023-07-28 20:57:43 +0600

    pre 89 done

--- src/views/dashboard/Settings.vue
+++ src/views/dashboard/Settings.vue
@@ -1,3 +1,23 @@
 <template>
-  <h2>Settings</h2>
+  <!-- <h2>Settings</h2> -->
+  <div class="tab">
+    <router-link to="/dashboard/settings/account">Account</router-link>
+    <router-link to="/dashboard/settings/website">Website</router-link>
+    <router-link to="/dashboard/settings/Theme">Theme</router-link>
+  </div>
+
+  <RouterView></RouterView>
 </template>
+
+<style>
+.tab a {
+  text-decoration: none;
+  padding: 3px 9px;
+  display: inline-block;
+  color: #333;
+}
+
+.tab a.router-link-active {
+  border-bottom: 3px solid var(--brand-color);
+}
+</style>
---
+++ src/views/dashboard/settings/Theme.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <div>
+    <h1>Theme Settings</h1>
+  </div>
+</template>
+
+<style scoped></style>
